75cc1d2147772b5410c6bec964f9363b723f212b
Zeya <50080904+ZeyaGun@users.noreply.github.com>
2021-12-15 14:52:47 +0900

    Coding: Transforming : Rotation

--- UdemyLecture/main.cpp
+++ UdemyLecture/main.cpp
@@ -12,6 +12,7 @@
 
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
+const float toRadians = 3.14159265f / 180.0f;
 
 GLuint VAO, VBO, shader, uniformModel;
 
@@ -19,6 +20,8 @@ bool direction = true;
 float triOffset = 0.0f;
 float triMaxoffset = 0.7f;
 float triIncrement = 0.005f; 
+
+float curAngle = 0;
 
 // Vertex Shader
 static const char* vShader = "													\n\
@@ -30,7 +33,7 @@ uniform mat4 model;																\n\
 																				\n\
 void main()																		\n\
 {																				\n\
-	gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, 0.4 * pos.z, 1.0);		\n\
+	gl_Position = model * vec4(pos.x * 0.4, pos.y * 0.4, pos.z, 1.0);		\n\
 }";
 
 // Fragment Shader
@@ -181,6 +184,7 @@ int main(int argc, char *argv[])
 	CreateTriangle();
 	CompileShaders();
 
+
 	// Loop until Window Closed
 	while (!glfwWindowShouldClose(mainWindow))
 	{
@@ -201,6 +205,12 @@ int main(int argc, char *argv[])
 			direction = !direction;
 		}
 
+		curAngle += 0.001f;
+		if (curAngle >= 360)
+		{
+			curAngle -= 360;
+		}
+		 
 		// Clear Window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
@@ -208,8 +218,10 @@ int main(int argc, char *argv[])
 		glUseProgram(shader);
 
 		glm::mat4 model(1.0f);
-		model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
-
+		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		model = glm::rotate(model, curAngle, glm::vec3(0.0f, 0.0f, 1.0f));
+		
+		
 
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
